26b6e79ef2c5622d0ce5c52711bed077877db1d7
Sumitdasss <sumitdas46123@gmail.com>
2026-08-14 16:53:33 +0600

    add

--- app/Studentlogin/StudentProfile/page.jsx
+++ app/Studentlogin/StudentProfile/page.jsx
@@ -35,7 +35,7 @@ useEffect(() => {
 
 const loadProfile = async (id) => {
   try {
-    const res = await fetch(`https://my-school-backend-iota.vercel.app/api/studentlogin/getstudentlogindeta?id=${student.id}`);
+    const res = await fetch(`https://my-school-backend-iota.vercel.app/api/studentlogin/getstudentlogindeta?id=${id}`);
     const data = await res.json();
      setUserData(data.data)
   } catch (error) {
